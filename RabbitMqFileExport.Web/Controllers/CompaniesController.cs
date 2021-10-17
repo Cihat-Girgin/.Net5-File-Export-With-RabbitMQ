7ac614922a22861747957e43c94e00d32b2724ee
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-17 12:38:22 +0300

    added secret.json

--- RabbitMqFileExport.Web/Controllers/CompaniesController.cs
+++ RabbitMqFileExport.Web/Controllers/CompaniesController.cs
@@ -1,7 +1,21 @@
-﻿namespace RabbitMqFileExport.Web.Controllers
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMqFileExport.Web.Controllers
 {
-    public class CompaniesController
+    public class CompaniesController : Controller
     {
-        
+        private readonly DatabaseContext _context;
+
+        public CompaniesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_context.Companies.ToListAsync());
+        }
     }
 }
