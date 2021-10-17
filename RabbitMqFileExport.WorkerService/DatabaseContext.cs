6d3a7bd429437d0d3a5dea0e1241eae14ccaddb3
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-17 16:11:24 +0300

    Create database contex in worker

---
+++ RabbitMqFileExport.WorkerService/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitMqFileExport.Model;
+
+namespace RabbitMqFileExport.WorkerService
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ExcelFile> ExcelFiles { get; set; }
+    }
+}
