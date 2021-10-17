52abbd473ea6925f8d259608198448b3fc2551fa
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-17 16:07:18 +0300

    Add client service in worker service

---
+++ RabbitMqFileExport.WorkerService/RabbitMqServices/RabbitMqClientService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace RabbitMqFileExport.WorkerService.RabbitMqServices
+{
+    public class RabbitMqClientService
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        public static string QueueName = "queue-excel-file";
+
+        private readonly ILogger<RabbitMqClientService> _logger;
+
+        public RabbitMqClientService(ConnectionFactory connectionFactory, ILogger<RabbitMqClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            if (_channel is { IsOpen: true })
+            {
+                return _channel;
+            }
+            _channel = _connection.CreateModel();
+            _logger.LogInformation("Connected to RabbitMQ ");
+           
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+
+            _connection?.Close();
+            _connection?.Dispose();
+
+            _logger.LogInformation("Disconnected to RabbitMQ");
+
+        }
+    }
+}
