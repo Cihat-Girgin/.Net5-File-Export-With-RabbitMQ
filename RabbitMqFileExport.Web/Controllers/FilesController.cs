e60540c3d7006a3162d0ece95605ba9d676901e2
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-17 15:14:40 +0300

    Add filescontroller

--- RabbitMqFileExport.Web/Controllers/FilesController.cs
+++ RabbitMqFileExport.Web/Controllers/FilesController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMqFileExport.Model;
 
 namespace RabbitMqFileExport.Web.Controllers
 {
     public class FilesController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly DatabaseContext _context;
+
+        public FilesController(DatabaseContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ExcelFiles.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(string fileName)
+        {
+            await _context.ExcelFiles.AddAsync(new ExcelFile {Name = $"{fileName}_{Guid.NewGuid().ToString().Substring(1, 15)}.xlsx"});
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
