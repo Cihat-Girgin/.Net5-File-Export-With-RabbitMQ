457fe15cc700a5230145713b75a0111fbeab06fb
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-17 16:12:42 +0300

    Update Program.cs

--- RabbitMqFileExport.WorkerService/Program.cs
+++ RabbitMqFileExport.WorkerService/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMqFileExport.WorkerService.RabbitMqServices;
 
 namespace RabbitMqFileExport.WorkerService
 {
@@ -16,6 +20,19 @@ namespace RabbitMqFileExport.WorkerService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var Configuration = hostContext.Configuration;
+
+                    services.AddDbContext<DatabaseContext>(options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+                    });
+
+
+                    services.AddSingleton<RabbitMqClientService>();
+                    services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetSection("RabbitMQ").Value), DispatchConsumersAsync = true });
+                    services.AddHostedService<Worker>();
+                });
     }
 }
